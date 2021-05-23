b2a29e4e19f855d53369e8142b7180caa070b174
olga148 <82608323+olga148@users.noreply.github.com>
2021-05-23 16:32:18 +0300

    Задание 2. Доработка решения "вычислить расстояние между двумя точками"

--- sandbox/src/main/java/Point.java
+++ sandbox/src/main/java/Point.java
@@ -5,18 +5,21 @@ public class Point {
     public double y2;
 
     public static void main (String[] args) {
-        Point p1 = new Point(4,10,0,13);
-        System.out.println("Расстояние между двумя точками " + p1.x1 + ";" + p1.y1 + " и " + p1.x2 + ";" + p1.y2 + " = " + p1.distance());
+        Point p1 = new Point(4, 10);
+        Point p2 = new Point();
+
+        p2.x2 = 0;
+        p2.y2 = 13;
+
+        System.out.println("Расстояние между двумя точками " + p1.x1 + ";" + p1.y1 + " и " + p1.x2 + ";" + p1.y2 + " = " + p1.distance(p2));
     }
 
-    public double distance(){
-        return Math.sqrt((this.x2 - this.x1) * (this.x2 - this.x1) + (this.y2 - this.y1) * (this.y2 - this.y1));
+    public double distance(Point p2){
+        return Math.sqrt((p2.x2 - this.x1) * (p2.x2 - this.x1) + (p2.y2 - this.y1) * (p2.y2 - this.y1));
     }
 
-    public Point(double x1, double y1, double x2, double y2) {
+    public Point(double x1, double y1) {
         this.x1 = x1;
         this.y1 = y1;
-        this.x2 = x2;
-        this.y2 = y2;
     }
 }
