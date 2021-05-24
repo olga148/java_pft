089dd255b1313b192ee709eee5b732764287dc95
olga148 <82608323+olga148@users.noreply.github.com>
2021-05-24 23:00:50 +0300

    Доработано задание 2
    Вместо создания четырех точек, создала 2

--- sandbox/src/main/java/Point.java
+++ sandbox/src/main/java/Point.java
@@ -1,25 +1,20 @@
 public class Point {
-    public double x1;
-    public double y1;
-    public double x2;
-    public double y2;
-
+    public double x;
+    public double y;
+    
     public static void main (String[] args) {
         Point p1 = new Point(4, 10);
         Point p2 = new Point(0, 13);
 
-        p2.x2 = 0;
-        p2.y2 = 13;
-
-        System.out.println("Расстояние между двумя точками " + p1.x1 + ";" + p1.y1 + " и " + p2.x2 + ";" + p2.y2 + " = " + p1.distance(p2));
+        System.out.println("Расстояние между двумя точками " + p1.x + ";" + p1.y + " и " + p2.x + ";" + p2.y + " = " + p1.distance(p2));
     }
 
     public double distance(Point p2){
-        return Math.sqrt((p2.x2 - this.x1) * (p2.x2 - this.x1) + (p2.y2 - this.y1) * (p2.y2 - this.y1));
+        return Math.sqrt((p2.x - this.x) * (p2.x - this.x) + (p2.y - this.y) * (p2.y - this.y));
     }
 
-    public Point(double x1, double y1) {
-        this.x1 = x1;
-        this.y1 = y1;
+    public Point(double x, double y) {
+        this.x = x;
+        this.y = y;
     }
 }
