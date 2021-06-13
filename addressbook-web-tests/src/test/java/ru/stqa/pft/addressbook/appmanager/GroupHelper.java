3cc234548c1fb0cc1a4b92b2d8e6717632d41824
olga148 <82608323+olga148@users.noreply.github.com>
2021-06-13 12:39:03 +0700

    Создание теста Удаление группы

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/GroupHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/GroupHelper.java
@@ -12,6 +12,7 @@ public class GroupHelper extends HelperBase {
     }
 
     public void returnToGroupPage() {
+
         click(By.linkText("group page"));
     }
 
@@ -27,6 +28,15 @@ public class GroupHelper extends HelperBase {
     }
 
     public void initGroupCreation() {
+
         click(By.name("new"));
+    }
+
+    public void selectGroup() {
+        click(By.name("selected[]"));
+    }
+
+    public void deleteSelectedGroups() {
+        click(By.name("delete"));
     }
 }
