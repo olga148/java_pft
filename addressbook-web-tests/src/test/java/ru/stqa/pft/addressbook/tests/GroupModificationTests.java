2f59d835db9f1425d97679ce15b6c8b86586e0bd
olga148 <82608323+olga148@users.noreply.github.com>
2021-06-21 22:14:31 +0700

    Проверка и обеспечение предусловия для теста модификация группы

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
@@ -8,9 +8,12 @@ public class GroupModificationTests extends TestBase {
     @Test
     public void testGroupModification() {
         app.getNavigationHelper().gotoGroupPage();
+        if (!app.getGroupHelper().isThereAGroup()) {
+            app.getGroupHelper().createGroup(new GroupData("zzz", "test11", "test22"));
+        }
         app.getGroupHelper().selectGroup();
         app.getGroupHelper().initGroupModification();
-        app.getGroupHelper().fillGroupForm(new GroupData("zzz", "test11", "test22"));
+        app.getGroupHelper().fillGroupForm(new GroupData("test", "example", "fill11"));
         app.getGroupHelper().submitGroupModification();
         app.getGroupHelper().returnToGroupPage();
     }
