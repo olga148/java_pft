65560510d8802305150b60731798fef8df5b96f8
olga148 <82608323+olga148@users.noreply.github.com>
2021-06-23 22:48:38 +0700

    контроль количества элементов для модификации контакта

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationTests.java
@@ -8,6 +8,7 @@ public class ContactModificationTests extends TestBase {
     @Test
     public void testContactModification() {
         app.getNavigationHelper().gotoHomePage();
+        int before = app.getContactHelper().getContactCount();
         if (!app.getContactHelper().isThereAContact()) {
             app.navigationHelper.gotoContactPage(app);
             app.getContactHelper().createContact(new ContactData("olga", "shakhova", "+79904459058", "olga@mail.com", "testolga@gmail.com", "testingolga@mail.com"));
@@ -16,5 +17,7 @@ public class ContactModificationTests extends TestBase {
         app.getContactHelper().fillContactForm(new ContactData("olya", "tester", "+79904459056", "olga32@mail.com", "testolga21@gmail.com", "testingolga44@mail.com"));
         app.getContactHelper().submitContactModification();
         app.getContactHelper().returnToHomePage();
+        int after = app.getContactHelper().getContactCount();
+        Assert.assertEquals(after, before);
     }
 }
